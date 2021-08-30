38dff2881204e560dd0fbb8584ac7a8835f2bccb
Mohammad <malnajjar16@gmail.com>
2021-08-30 14:00:39 +0200

    functions for the submit form are made

--- src/App.js
+++ src/App.js
@@ -24,13 +24,40 @@ const initialExpenses = [
 ];
 function App() {
   const [expenses, setExpenses] = useState(initialExpenses);
+  const [charge, setCharge] = useState('');
+  const [amount, setAmount] = useState('');
 
+  function handleCharge(e) {
+    setCharge(e.target.value);
+  }
+  function handleAmount(e) {
+    setAmount(+e.target.value);
+  }
+  function submitForm(e) {
+    e.preventDefault();
+    setExpenses((prevState) => {
+      return [
+        ...prevState,
+        {
+          id: uuidv4(),
+          charge,
+          amount,
+        },
+      ];
+    });
+  }
   return (
     <>
       <Alert />
       <h1>Budget calculator</h1>
       <main className='App'>
-        <ExpenseForm />
+        <ExpenseForm
+          charge={charge}
+          amount={amount}
+          handleCharge={handleCharge}
+          handleAmount={handleAmount}
+          submitForm={submitForm}
+        />
         <ExpenseList expenses={expenses} />
       </main>
       <h1>
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { MdSend } from 'react-icons/md';
-const ExpenseForm = () => {
+const ExpenseForm = ({
+  charge,
+  amount,
+  handleAmount,
+  handleCharge,
+  submitForm,
+}) => {
   return (
-    <form>
+    <form onSubmit={submitForm}>
       <div className='form-center'>
         <div className='form-group'>
           <label htmlFor='charge'>Charge</label>
           <input
+            onChange={handleCharge}
             type='text'
             name='charge'
             id='charge'
             className='form-control'
             placeholder='e.g rent'
+            value={charge}
           />
         </div>
         <div className='form-group'>
@@ -19,11 +27,13 @@ const ExpenseForm = () => {
             Amount
           </label>
           <input
+            onChange={handleAmount}
             type='number'
             name='amount'
             id='amount'
             className='form-control'
             placeholder='e.g 1000'
+            value={amount}
           />
         </div>
       </div>
